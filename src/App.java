5d276c83ad9f620c976379fd347464319f2f46a8
DragonautMk <michelesavarese1997@gmail.com>
2025-01-16 13:21:09 +0100

    lezione 2 java

---
+++ src/App.java
@@ -0,0 +1,34 @@
+
+public class App {
+
+    // public static void main(String[] args) throws Exception {
+    //     // System.out.println("Hello, World!");
+    //     info("Alessandro", "Scaccia");
+    //     info("Piero", "Nicodemo");
+    // }
+    // public static void info() {
+    //     String name = "Jotaro";
+    //     String surname = "Kujo";
+    //     System.out.println(name + " " + surname);
+    // }
+    // public static void info() {
+    //     String name1 = "Nicoletta";
+    //     String surname1 = "Barbambella";
+    //     System.out.println(name1 + " " + surname1);
+    // }
+    // public static void info(String name, String surname) {
+    //     System.out.println("\n Hello ma babi " + name + " " + surname);
+    // }
+    public static void main(String[] args) throws Exception {
+        // System.out.println();
+        Person p = new Person();
+//! Tramite la dot notation possiamo entrare all'interno di un oggetto.
+        p.name = "Andrea";
+        p.surname = "Nicoletta";
+        p.age = 18;
+
+        p.introduceYourSelf();
+    }
+
+
+}
---
+++ src/Person.java
@@ -0,0 +1,19 @@
+
+public class Person {
+
+    public String name;
+    public String surname;
+    public int age;
+
+    public Person() {
+    }
+
+//!Metodi
+    public void introduceYourSelf() {
+        System.out.println(this.name + " " + this.surname + ", " + this.age + " anni.");
+    }
+
+
+
+//! this non obbligatorio in java
+}
